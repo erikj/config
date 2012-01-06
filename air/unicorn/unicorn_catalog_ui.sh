68241fca9325321c11f42f3b5b940a05d08df40f
erikj <root@erikj.info>
2012-01-05 19:03:33 -0700

    adding unicorn init for air

---
+++ air/unicorn/unicorn_catalog_ui.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# /opt/local/sbin/unicorn_catalog_ui.sh
+
+rack_env='development'
+port=8080
+
+if [ $1 ]; then
+  rack_env=$1
+fi
+
+if [ $2 ]; then
+  port=$2
+fi
+
+rails_root="/Users/molecule/Dropbox/UCAR/catalog_ui"
+
+bundle="/Users/molecule/.rvm/gems/ruby-1.8.7-p352/bin/bundle"
+
+# http://beginrescueend.com/workflow/scripting/
+
+# Load RVM into a shell session *as a function*
+if [[ -s /Users/molecule/.rvm/scripts/rvm ]] ; then
+  # First try to load from a user install
+  source /Users/molecule/.rvm/scripts/rvm
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  # Then try to load from a root install
+  source "/usr/local/rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+fi
+
+cd $rails_root
+
+exe="$bundle exec unicorn_rails -E $rack_env -p $port"
+
+echo $exe
+
+$exe
